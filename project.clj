96085561b07f88e9f967bcc87e79f761686c2c1c
John Wiseman <jwiseman@aether.com>
2014-08-31 15:56:37 -0700

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject lein-awsuberwar "0.1.1-SNAPSHOT"
+(defproject lein-awsuberwar "0.1.1"
   :description "Creates an uberwar that includes .ebextensions, if it exists."
   :url "https://github.com/wiseman/lein-awsuberwar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories {"releases" :clojars}
   :eval-in-leiningen true
   :plugins [[lein-ring "0.8.11"]]
   :dependencies [[lein-ring "0.8.11"]])
